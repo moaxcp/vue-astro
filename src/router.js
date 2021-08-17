bb5c56263eb1bc4001e36653999670325694bdf6
John Mercier <moaxcp@gmail.com>
2021-08-17 11:59:24 -0400

    fixing base for router

--- src/router.js
+++ src/router.js
@@ -9,6 +9,9 @@ import SwitchGuide from "@/views/guide/SwitchGuide";
 Vue.use(Router)
 
 const router = new Router({
+  base: process.env.NODE_ENV === 'production'
+    ? '/vue-astro/'
+    : '/',
   mode: 'history',
   routes: [
     {
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: '/vue-astro/'
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/vue-astro/'
+        : '/'
 }
